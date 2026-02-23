734ab0849dc3769ea3002d1e56c8f300e786dbcd
astrandandrea <91681425+astrandandrea@users.noreply.github.com>
2026-02-23 11:35:21 +0100

    start på main

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 // hej!
 
 //heej
+
+d3.json("sw/starwars-episode-2-interactions-allCharacters.json")
+  .then(function(data) {
+
+    const nodes = data.nodes;
+    const links = data.links;
+
+
+});
