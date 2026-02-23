672c20ced03feb4510edfe9e112155810c4b7c01
astrandandrea <91681425+astrandandrea@users.noreply.github.com>
2026-02-23 15:39:36 +0100

    ingen option 1 elelr 2

--- main.js
+++ main.js
@@ -1,45 +1,137 @@
-d3.json("sw/starwars-episode-2-interactions-allCharacters.json")
+const fileMap = {
+    "all": "starwars-interactions/starwars-full-interactions-allCharacters.json",
+    "1": "starwars-interactions/starwars-episode-1-interactions-allCharacters.json",
+    "2": "starwars-interactions/starwars-episode-2-interactions-allCharacters.json",
+    "3": "starwars-interactions/starwars-episode-3-interactions-allCharacters.json",
+    "4": "starwars-interactions/starwars-episode-4-interactions-allCharacters.json",
+    "5": "starwars-interactions/starwars-episode-5-interactions-allCharacters.json",
+    "6": "starwars-interactions/starwars-episode-6-interactions-allCharacters.json",
+    "7": "starwars-interactions/starwars-episode-7-interactions-allCharacters.json"
+};
 
-// hej!
+let selectedCharacter = null;
 
-.then(function(data) {
+//  dropdowns
+function setupDropdown(selectId, svgId) {
 
-    const width = 1000;
-    const height = 800;
+    const select = d3.select(selectId);
 
-    const nodes = data.nodes;
-    const links = data.links;
-
-    const svg = d3.select("#network");
-
-    const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id((d, i) => i))
-        .force("charge", d3.forceManyBody().strength(-200))
-        .force("center", d3.forceCenter(width / 2, height / 2));
-
-    const link = svg.select(".links")
-        .selectAll("line")
-        .data(links)
-        .join("line")
-        .attr("stroke", "#aaa");
-
-    const node = svg.select(".nodes")
-        .selectAll("circle")
-        .data(nodes)
-        .join("circle")
-        .attr("r", 5)
-        .attr("fill", "steelblue");
-
-    simulation.on("tick", function() {
-        link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
-
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
+    Object.keys(fileMap).forEach(key => {
+        select.append("option")
+            .attr("value", key)
+            .text(key === "all" ? "All Episodes" : "Episode " + key);
     });
 
-});
+    select.on("change", function() {
+        const file = fileMap[this.value];
+        drawNetwork(svgId, file);
+    });
+
+    drawNetwork(svgId, fileMap["all"]);
+}
+
+// graf
+function drawNetwork(svgId, filePath) {
+
+    d3.json(filePath).then(data => {
+
+        const nodes = data.nodes;
+        const links = data.links;
+
+        const svg = d3.select(svgId);
+        const width = svg.node().clientWidth;
+        const height = svg.node().clientHeight;
+
+        svg.selectAll("*").remove();
+
+        const linkLayer = svg.append("g");
+        const nodeLayer = svg.append("g");
+
+        const simulation = d3.forceSimulation(nodes)
+
+            .force("link",
+                d3.forceLink(links)
+                  .id((d,i) => i)
+                  .distance(30)
+            )
+
+            .force("charge", d3.forceManyBody().strength(-40))
+
+            .force("center", d3.forceCenter(width/2, height/2))
+
+            .force("collision", d3.forceCollide().radius(8))
+
+            .on("tick", ticked);
+
+
+        // rita lnjer
+        const link = linkLayer.selectAll("line")
+            .data(links)
+            .join("line")
+            .attr("stroke", "#999")
+            .attr("stroke-width", d => Math.sqrt(d.value) * 0.5)
+
+            .append("title")
+            .text(d =>
+                nodes[d.source.index].name +
+                " – " +
+                nodes[d.target.index].name +
+                "\nScenes: " + d.value
+            );
+
+        // rita noder
+        const node = nodeLayer.selectAll("circle")
+            .data(nodes)
+            .join("circle")
+            .attr("r", d => 4 + Math.sqrt(d.value))
+            .attr("fill", d => d.colour)
+
+            .append("title")
+            .text(d =>
+                d.name +
+                "\nScenes: " + d.value
+            );
+
+
+        const circles = nodeLayer.selectAll("circle");
+
+        // brush + link vid klick
+        circles.on("click", function(event, d) {
+
+            selectedCharacter = d.name;
+
+            // Markera noder i båda grafer
+            d3.selectAll("circle")
+                .classed("highlight", node =>
+                    node.name === selectedCharacter
+                );
+        });
+
+
+        function ticked() {
+
+            linkLayer.selectAll("line")
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
+        
+            circles
+                .attr("cx", d => {
+                    const r = 4 + Math.sqrt(d.value);
+                    d.x = Math.max(r, Math.min(width - r, d.x));
+                    return d.x;
+                })
+                .attr("cy", d => {
+                    const r = 4 + Math.sqrt(d.value);
+                    d.y = Math.max(r, Math.min(height - r, d.y));
+                    return d.y;
+                });
+        }
+
+    });
+}
+
+
+setupDropdown("#select1", "#view1");
+setupDropdown("#select2", "#view2");
